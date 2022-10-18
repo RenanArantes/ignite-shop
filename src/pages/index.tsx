f0fded14e3413231bccaf594a8ab57284b6a11a5
RenanArantes <malevo18@gmail.com>
2022-10-18 14:20:54 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,30 @@
+import Image from "next/future/image"
 import { styled } from "../styles"
+import { HomeContaier, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 8,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: "bolder"
-  },
-
-  '&:hover': {
-    filter: 'brightness(4)'
-  }
-})
+import shirt1 from '../assets/shirts/shirt1.png'
+import shirt2 from '../assets/shirts/shirt2.png'
+import shirt3 from '../assets/shirts/shirt3.png'
 
 export default function Home() {
-  return <Button onClick={() => alert('AAA')}><span>Hello</span> Next</Button>
+  return (
+    <HomeContaier>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContaier>
+  )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,6 +14,6 @@ export const globalStyles = globalCss({
 
   'body, input, textarea, button': {
     fontFamily: 'Roboto',
-    fontWeight: 400
+    fontWeight: 400,
   }
 })
