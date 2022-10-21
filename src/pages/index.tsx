7f3b97e542d8e9ac7745c0f945c4b6d46ac20f86
RenanArantes <malevo18@gmail.com>
2022-10-21 08:56:44 -0300

    change link to slider inside

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,16 +34,16 @@ export default function Home({ products }: HomeProps) {
       {
         products.map(product => {
           return (
-            <Link href={`/product/${product.id}`} key={product.id}>
-              <Product className="keen-slider__slide">
+            <Product key={product.id} className="keen-slider__slide">
+              <Link href={`/product/${product.id}`} >
                 <Image src={product.imageUrl} width={520} height={480} alt="" />
         
                 <footer>
                   <strong>{product.name}</strong>
                   <span>{product.price}</span>
                 </footer>
-              </Product>
-            </Link>
+              </Link>
+            </Product>
             
           )
         })
